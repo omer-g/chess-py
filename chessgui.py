c9e97982735782040a4df23a701a4eb76888ab67
OD <omer.d@campus.technion.ac.il>
2021-05-19 00:16:48 +0300

    Add GuiSquare class

--- chessgui.py
+++ chessgui.py
@@ -1,47 +1,101 @@
+from pychess import Bishop, Piece, Queen, Square
 from PyQt5 import QtWidgets
 import PyQt5.QtWidgets as qtw
 import PyQt5.QtGui as qtg
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
+from enum import Enum, auto
 
-DIM = 1024
-SQUARE = 70
-PIECE = 60
+DIM = 700
+SQUARE_SIZE = 70
+PIECE_SIZE = 60
+DARK_COLOR = "grey"
+BRIGHT_COLOR = "white"
+
+class Pieces(Enum):
+    Pawn = auto(),
+    Rook = auto(),
+    Knight = auto(),
+    Bishop = auto(),
+    Queen = auto(),
+    King = auto(),
+    Empty = auto()
+
+class Colors(Enum):
+    Black = auto(),
+    White = auto()
+
+PIECE_TO_PNG = {
+    Pieces.Pawn: "pawn.png",
+    Pieces.Rook: "rook.png",
+    Pieces.Knight: "knight.png",
+    Pieces.Bishop: "bishop.png",
+    Pieces.Queen: "queen.png",
+    Pieces.King: "king.png"
+    }
+
+COLOR_TO_PNG = {
+        Colors.Black: "b_",
+        Colors.White: "w_"
+        }
 
 
-# class GuiSquare():
-#     def __init__(self, x, y, square_color):
-#         _square = QFrame(self)
+# @param board_position: bottom left corner tuple (x, y)
+# @param r, c: row and col  
+class GuiSquare():
+    def __init__(self, parent_window, board_position, r, c):
+        piece = None
+        self.x = board_position[0] + c * SQUARE_SIZE
+        self.y = board_position[1] - r * SQUARE_SIZE
+        square = QFrame(parent_window)
+        square.setObjectName(u"frame" + f"{r}-{c}")
+        square.setGeometry(QRect(self.x, self.y, SQUARE_SIZE, SQUARE_SIZE))
+        if (r + c) % 2 == 0:
+            square.setStyleSheet(f"background-color: {DARK_COLOR};")
+        else:
+            square.setStyleSheet(f"background-color: {BRIGHT_COLOR};")
 
-class MainWindow(qtw.QWidget):
+    # @param parent_window: pass main window
+    # @param piece: tuple of the enums Colors and Pieces
+    # @param position: position of square (x, y)
+    def place_piece(self, parent_window, piece_tuple):
+        color, kind = piece_tuple
+
+        self.piece = QtWidgets.QLabel(parent_window)
+        self.piece.setPixmap(qtg.QPixmap(f"pieces/{COLOR_TO_PNG[color]}{PIECE_TO_PNG[kind]}"))
+        self.piece.setGeometry(self.x + 5 , self.y + 5, PIECE_SIZE, PIECE_SIZE)
+        self.piece.show()
+                
+
+class BoardWindow(qtw.QWidget):
     def __init__(self):
         super().__init__()
         self.setLayout(qtw.QGridLayout())
         self.resize(DIM,DIM)        
-        
-        board = [[] for _ in range(0, 8)]
-        for r, row in enumerate(board):
-            for c in range(0, 8):
-                square = QFrame(self)
-                square.setObjectName(u"frame" + f"{r}-{c}")
-                square.setGeometry(QRect(DIM // 2 - SQUARE * 4 + SQUARE * c,
-                                        DIM - 300 - SQUARE*(r+1), SQUARE, SQUARE))
-                if (r + c) % 2 == 0:
-                    square.setStyleSheet(u"background-color: grey;")
-                else:
-                    square.setStyleSheet(u"background-color: white;")
-                row.append(square)
-        self.setStyleSheet("MainWindow {background-color: rgb(0,139,139);}") 
-        
+        self.setStyleSheet("BoardWindow {background-color: rgb(0,139,139);}") 
         self.setWindowFlag(Qt.FramelessWindowHint)
+        self.board_position = (DIM // 2 - SQUARE_SIZE * 4, DIM // 2 + SQUARE_SIZE * 3)
+        
+        # Draw board
+        self.board = [[] for _ in range(0, 8)]
+        for r, row in enumerate(self.board):
+            for c in range(0, 8):
+                row.append(GuiSquare(self, self.board_position, r, c))
 
-
-        pawn = QtWidgets.QLabel(self)
-        pawn.setPixmap(qtg.QPixmap("pieces/b_pawn.png"))
-        print(board[0][0].pos())
-        pawn.setGeometry(board[3][0].pos().x() + 5 , board[3][0].pos().y() + 5, PIECE, PIECE)
         self.show()
 
+    # This will accept a board state and set pieces accordingly.
+    def set_pieces(self):
+        for square in self.board[1]:
+            square.place_piece(self, (Colors.White, Pieces.Pawn))
+        for square in self.board[6]:
+            square.place_piece(self, (Colors.Black, Pieces.Pawn))
+
+
+
 app = qtw.QApplication([])
-mw = MainWindow()
+mw = BoardWindow()
+mw.set_pieces()
 app.exec_()
+
+
